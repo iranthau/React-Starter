08378afbad4c7ebbccec88c77dec6d8c866da37f
Iran R <iranthau@gmail.com>
2015-11-19 00:10:05 +1100

    Fix routing issue

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ port = 8000,
 cookieParser = require('cookie-parser'),
 bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true,
